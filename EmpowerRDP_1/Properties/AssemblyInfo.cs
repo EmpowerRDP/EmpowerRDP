76d87bffa92b2a2e6553bee22f24d89ee1e96c2f
Rene De Posada <107321636+rdpslc@users.noreply.github.com>
2023-05-13 10:20:36 -0700

    Add project files.

---
+++ EmpowerRDP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerRDP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerRDP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8503B843-138C-421C-859A-7679F6B3F36C")]
+[assembly: AssemblyVersion("1.0.0.0")]
